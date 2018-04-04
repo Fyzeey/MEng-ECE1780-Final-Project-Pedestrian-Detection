a88552e45e3bac7f083ffca100d9e5f759a15172
Yinze Fan <fanyinze@gmail.com>
2018-04-03 23:14:26 -0400

    Change layers to for 128 x 128 resolution

--- convolutional_model.py
+++ convolutional_model.py
@@ -7,15 +7,25 @@ class ConvModel:
         self.input_placeholder = tf.placeholder(tf.float32, shape=[None] + resolution + [channels],
                                                 name='image')
 
-        x = tf.layers.conv2d(inputs=self.input_placeholder,
+       x = tf.layers.conv2d(inputs=self.input_placeholder,
                              filters=32,
                              padding='same',
                              kernel_size=5) # 64
+
+        x = tf.layers.conv2d(inputs=x,
+                             filters=32,
+                             padding='same',
+                             kernel_size=5)
 
         x = tf.layers.max_pooling2d(inputs=x,
                                     pool_size=2,
                                     strides=2)   # 32
         # ==================================================
+
+        x = tf.layers.conv2d(inputs=x,
+                             filters=64,
+                             padding='same',
+                             kernel_size=5)
 
         x = tf.layers.conv2d(inputs=x,
                              filters=64,
@@ -30,37 +40,44 @@ class ConvModel:
         x = tf.layers.conv2d(inputs=x,
                              filters=128,
                              padding='same',
-                             kernel_size=4)
+                             kernel_size=5)
 
-        '''x = tf.layers.conv2d(inputs=x,
+        x = tf.layers.conv2d(inputs=x,
                              filters=128,
                              padding='same',
-                             kernel_size=4)'''
+                             kernel_size=4)
 
         x = tf.layers.max_pooling2d(inputs=x,
                                     pool_size=2,
                                     strides=2)  # 8
-        # ===================================================
 
         x = tf.layers.conv2d(inputs=x,
                              filters=256,
                              padding='same',
-                             kernel_size=4)
+                             kernel_size=5)
 
-        '''x = tf.layers.conv2d(inputs=x,
+        x = tf.layers.conv2d(inputs=x,
                              filters=256,
                              padding='same',
-                             kernel_size=4)'''
+                             kernel_size=5)
 
         x = tf.layers.max_pooling2d(inputs=x,
                                     pool_size=2,
-                                    strides=2)  # 4
+                                    strides=2)  # 16
 
         # ===================================================
         x = tf.reshape(x, shape=[-1, x.shape[1]*x.shape[2]*x.shape[3]])
 
         x = tf.layers.dense(inputs=x,
                             units=2048,
+                            activation=tf.nn.relu)
+
+        x = tf.layers.dense(inputs=x,
+                            units=512,
+                            activation=tf.nn.relu)
+
+        x = tf.layers.dense(inputs=x,
+                            units=128,
                             activation=tf.nn.relu)
 
         x = tf.layers.dense(inputs=x,
